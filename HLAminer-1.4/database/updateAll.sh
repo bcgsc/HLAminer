8b38c6545ac424385a8764f541e3cc61b06a6609
René <rwarren@bcgsc.ca>
2022-09-15 11:25:07 -0700

    Update updateAll.sh

--- HLAminer-1.4/database/updateAll.sh
+++ HLAminer-1.4/database/updateAll.sh
@@ -3,3 +3,5 @@
 ./updateHLA-I_II_coding.sh
 ./updateHLA-I_II_genomic.sh
 ./update_p_designation.sh
+cat GCA_000001405.15_GRCh38_genomic.chr-only-noChr6.fa HLA-I_II_GEN.fasta | pigz - > GCA_000001405.15_GRCh38_genomic.chr-only-noChr6-HLA-I_II_GEN.fa.gz
+cat GCA_000001405.15_GRCh38_genomic.chr-only-noChr6.fa HLA-I_II_CDS.fasta | pigz - > GCA_000001405.15_GRCh38_genomic.chr-only-noChr6-HLA-I_II_CDS.fa.gz
