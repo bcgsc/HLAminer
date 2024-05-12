29e62f283ff2da06ffa6f349a79e2d9599da17f1
René <rwarren@bcgsc.ca>
2024-05-12 06:58:54 -0700

    Update HPRArnaseq_pacbioSEclassI.sh
    removed hardcoded path
    updated location of isoseq

--- HLAminer-1.4/bin/HPRArnaseq_pacbioSEclassI.sh
+++ HLAminer-1.4/bin/HPRArnaseq_pacbioSEclassI.sh
@@ -2,11 +2,11 @@
 #echo "Building bwa index..."
 #/gsc/btl/linuxbrew/bin/bwa index HLA_ABC_CDS.fasta
 echo "Downloading MCF-7 pacbio RNA-seq reads..."
-wget http://datasets.pacb.com.s3.amazonaws.com/2013/IsoSeqHumanMCF7Transcriptome/IsoSeq_MCF7_polished.unimapped.fasta
+wget http://www.bcgsc.ca/downloads/btl/hlaminer/IsoSeq_MCF7_polished.unimapped.fasta
 echo "Running bwa mem..."
-/gsc/btl/linuxbrew/bin/bwa mem -x pacbio ../database_bwamem/HLA_ABC_CDS.fasta IsoSeq_MCF7_polished.unimapped.fasta > MCF7_vs_HLA.sam
+bwa mem -x pacbio ../database_bwamem/HLA_ABC_CDS.fasta IsoSeq_MCF7_polished.unimapped.fasta > MCF7_vs_HLA.sam
 echo "Fixing MD tag..."
-/gsc/btl/linuxbrew/bin/samtools fillmd -S MCF7_vs_HLA.sam ../database_bwamem/HLA_ABC_CDS.fasta > MCF7_vs_HLAmd.sam
+samtools fillmd -S MCF7_vs_HLA.sam ../database_bwamem/HLA_ABC_CDS.fasta > MCF7_vs_HLAmd.sam
 ### Predict HLA
 echo "Predicting HLA..."
 ../bin/HLAminer.pl -a MCF7_vs_HLAmd.sam -h ../database_bwamem/HLA_ABC_CDS.fasta -s 500 -q 1 -i 1 -e 1
