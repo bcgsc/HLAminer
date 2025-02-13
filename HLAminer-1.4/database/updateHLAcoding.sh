cfa74f0ca399e13b7dd13e707e2382171aa0fcae
René <rwarren@bcgsc.ca>
2025-02-13 07:20:32 -0800

    remove hardcoded paths

--- HLAminer-1.4/database/updateHLAcoding.sh
+++ HLAminer-1.4/database/updateHLAcoding.sh
@@ -4,4 +4,4 @@ wget ftp://ftp.ebi.ac.uk/pub/databases/imgt/mhc/hla/fasta/B_nuc.fasta
 wget ftp://ftp.ebi.ac.uk/pub/databases/imgt/mhc/hla/fasta/C_nuc.fasta
 cat A_nuc.fasta B_nuc.fasta C_nuc.fasta | perl -ne 'chomp;if(/\>\S+\s+(\S+)/){print ">$1\n";}else{print "$_\n";}' > HLA_ABC_CDS.fasta
 ../bin/formatdb -p F -i HLA_ABC_CDS.fasta
-/home/pubseq/BioSw/bwa/bwa-0.5.9/bwa index -a is HLA_ABC_CDS.fasta
+bwa index -a is HLA_ABC_CDS.fasta
